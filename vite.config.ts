a90ab2c77f97987f9ed130fcf6466e3a3aeaa830
Alexander Nortung <alex_nortung@live.dk>
2023-05-10 15:55:27 +0200

    Added demo build and github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,8 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import packageJson from './package.json';
-
-const getPackageName = () => {
-    return packageJson.name.split('/').pop() ?? packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-    try {
-        return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
-    } catch (err) {
-        throw new Error('Name property in package.json is missing.');
-    }
-};
-
-const fileName = {
-    es: `${getPackageName()}.mjs`,
-    cjs: `${getPackageName()}.cjs`,
-    iife: `${getPackageName()}.iife.js`,
-};
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    server: {
-        cors: { origin: '*' },
-    },
-    base: './',
+    /* root: 'demo', */
     build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/main.ts'),
-            name: getPackageNameCamelCase(),
-            formats: ['es', 'cjs', 'iife'],
-            fileName: (format) => fileName[format],
-        },
+        outDir: 'demo-dist',
     },
-});
+})
